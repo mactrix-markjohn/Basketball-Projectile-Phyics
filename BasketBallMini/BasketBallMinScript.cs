194d86a499eaf6211c24bddb1341063784a06bb6
Chukwuemeka John Okparaeke <41091907+mactrix-markjohn@users.noreply.github.com>
2023-01-19 17:53:57 +0100

    This files contain useful resources
    This files contains important resources such as UI mask, materials, shader and other thing for Unity experience

---
+++ BasketBallMini/BasketBallMinScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BasketBallMinScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI PointText;
+
+    private int point = 0;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PointText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePoint()
+    {
+        point++;
+        PointText.text = $"{point}";
+    }
+}
---
+++ BasketBallMini/BasketBallScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketBallScript : MonoBehaviour
+{
+    [SerializeField] private BasketBallMinScript basketBallMini;
+
+    private Vector3 initialPosition;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ring")
+        {
+            //update the score here
+            basketBallMini.UpdatePoint();
+        }
+        
+        if (other.gameObject.tag == "Ground")
+        {
+            // set the basket ball to the initial position once it hits the ground
+            
+            //transform.position = initialPosition;
+            /*gameObject.GetComponent<Rigidbody>().useGravity = false;
+            gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;*/
+        }
+    }
+}
